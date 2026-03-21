d1a5a0e3941aa42b068933bc056c1ca653be0092
thegreencrayon2 <thegreencrayon2@gmail.com>
2026-03-21 09:55:58 -0400

    Create javascript file

---
+++ script.js
@@ -0,0 +1,10 @@
+// global variables
+let totalScore = 0;
+let pharmScore = 0;
+let therapeuticScore = 0;
+let safetyScore = 0;
+
+// question objects
+const questions = [];
+
+const q1 = {question: "", image: "", options: [], rightAnswer: "", category: ""};
